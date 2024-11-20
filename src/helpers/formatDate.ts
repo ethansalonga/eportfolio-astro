ab4997f0dccbdacdfa1a8e5b5309e78ee3dc21ae
Ethan Salonga <ethansalonga28@gmail.com>
2024-11-20 18:15:48 -0500

    fix: formatDate helper function

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,7 +1,9 @@
 export function formatDate(date: string) {
+  const localDate = new Date(date)
+  const correctedDate = new Date(localDate.getTime() + localDate.getTimezoneOffset() * 60 * 1000)
   return new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
     month: 'short',
     day: 'numeric'
-  }).format(new Date(date))
+  }).format(new Date(correctedDate))
 }
